64244ed8087d48998a655c10fad764254843f433
pgdn42 <89749328+pgdn42@users.noreply.github.com>
2022-03-28 16:46:41 +0200

    Whipped

--- MP2.m
+++ MP2.m
@@ -6,6 +6,7 @@ testInt = input(prompt);
 testNr = trainDigits(:,:,testInt);
 testAns = trainAns(testInt);
 ima(testNr)
+figure()
 
 %16 x 16 matriser med nummer. 0 för vit pixel och 1 för svart.
 %vi ska ta och göra en algoritm för att hitta alla tex 7or när vi 
@@ -15,4 +16,11 @@ ima(testNr)
 
 %Närmaste granne metoden --> ???
 
-
+for i = 1:numel(testNr)
+    if (testNr(i) >= 0.5)
+        testNr(i) = 1;
+    else
+        testNr(i) = 0;
+    end
+end
+ima(testNr)
